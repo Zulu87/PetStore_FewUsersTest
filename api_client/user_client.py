eafab473cbd330cc777048f5a7003ea7164d153f
Zulu87 <pavlo.znak@gmail.com>
2021-11-08 18:23:32 +0200

    tests

--- api_client/user_client.py
+++ api_client/user_client.py
@@ -1,8 +1,8 @@
 from api_client.abstract_api_client import AbstractApiClient
 
 #АПІ клієнт для колання ендпоїнта User
-class CurrentUserClient(AbstractApiClient):
+class UserClient(AbstractApiClient):
     user_endpoint = "user"
 
     def add_user(self, data_payload, param = None):
-        return self._post(endpoint=CurrentUserClient.user_endpoint, payload = data_payload, param=param)
+        return self._post(endpoint=UserClient.user_endpoint, payload = data_payload, param=param)
--- test/tests.py
+++ test/tests.py
@@ -5,13 +5,13 @@ from api_client.user_client import *
 class TestUsers:
 
     # тест на додавання одного юзера
-    def test_user_is_added(self, current_user_client: CurrentUserClient, users_data_provider):
+    def test_user_is_added(self, current_user_client: UserClient, users_data_provider):
         single_user_request = current_user_client.add_user(data_payload = users_data_provider("single_user_data.csv"))
 
         assert single_user_request.status_code == 200, f"Request failed with the status code {single_user_request.status_code}"
 
     # тест на додавання кількох юзерів списком
-    def test_few_users_are_added(self, current_user_client: CurrentUserClient, users_data_provider):
+    def test_few_users_are_added(self, current_user_client: UserClient, users_data_provider):
         few_users_request = current_user_client.add_user(data_payload = users_data_provider("multiple_users_data.csv"), param="createWithList")
 
         assert few_users_request.status_code == 200, f"Request failed with the status code {few_users_request.status_code}"
